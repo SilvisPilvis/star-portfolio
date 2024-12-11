2178b594a23991afd1f4bcd744e2760fb26186be
Silvestrs <silvestrsl47@gmail.com>
2024-12-11 22:05:38 +0200

    Fixed Root Layout to Fit 100vh

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                text: '#e0fafe',
+                background: '#090d0e',
+                primary: '#69dffc',
+                secondary: '#a9032f',
+                accent: '#fbb325',
+                red: "#ff0000",
+            },
+        },
+    },
+    plugins: [],
 }
